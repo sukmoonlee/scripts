608b8e157dc31efc42f20274b3a6a4de96cce69d
Sukmoon Lee <smlee@sukmoonlee.com>
2020-09-15 13:49:33 +0900

    o 20200915 release  - openssl 1.1.1 지원 (openssl11 이용)  - Local Server에 대한 인증서 확인 기능 추가

--- cert/cert_check.sh
+++ cert/cert_check.sh
@@ -3,20 +3,23 @@
 # Certfication Check Script
 # 2019.09.17 created by CoreSolution (smlee@sk.com)
 ################################################################################
-SCRIPT_VERSION="20191027"
+SCRIPT_VERSION="20200915"
+LC_ALL=en_US.UTF-8
 LANG=en_US.UTF-8
 HOSTNAME=$(hostname)
 
-if [ -f "/usr/bin/openssl" ] ; then PG_OPENSSL="/usr/bin/openssl"
+if [ -f "/usr/bin/openssl11" ] ; then PG_OPENSSL="/usr/bin/openssl11"
+elif [ -f "/usr/bin/openssl" ] ; then PG_OPENSSL="/usr/bin/openssl"
 elif [ -f "/usr/local/bin/openssl" ] ; then PG_OPENSSL="/usr/local/bin/openssl"
 else echo "Error - Command not found 'openssl'"; exit 1; fi
-if [ -f "/bin/timeout" ] ; then PG_TIMEOUT="/bin/timeout";
-elif [ -f "/usr/bin/timeout" ] ; then PG_TIMEOUT="/usr/bin/timeout";
-elif [ -f "/usr/share/doc/bash-3.2/scripts/timeout" ] ; then PG_TIMEOUT="bash /usr/share/doc/bash-3.2/scripts/timeout";
+if [ -f "/bin/timeout" ] ; then PG_TIMEOUT="/bin/timeout"
+elif [ -f "/usr/bin/timeout" ] ; then PG_TIMEOUT="/usr/bin/timeout"
+elif [ -f "/usr/share/doc/bash-3.2/scripts/timeout" ] ; then PG_TIMEOUT="bash /usr/share/doc/bash-3.2/scripts/timeout"
 else echo "Error - Command not found 'timeout'"; exit 1; fi
 ################################################################################
 TARGET_HOST=
 TARGET_PORT=
+FLAG_ALL=0
 while [ "$#" -gt 0 ] ; do
 case "$1" in
 	-v|--version)
@@ -37,6 +40,14 @@ case "$1" in
 		set -x
 		shift 1
 		;;
+	-a|--all)
+		FLAG_ALL=1
+		shift 1
+		;;
+	--help)
+		echo "Usage: $0 -h {Hostname/IP} -p {Port}"
+		exit 1
+		;;
 	 *)
 		if [[ $1 =~ : ]] ; then
 			ARG=(${1/:/ })
@@ -50,11 +61,6 @@ case "$1" in
 		;;
 esac
 done
-
-if [ "$TARGET_HOST" == "" ] || [ "$TARGET_PORT" == "" ] ; then
-	echo "Usage: $0 -h {Hostname/IP} -p {Port}"
-	exit 1
-fi
 ################################################################################
 function StringCat
 {
@@ -69,7 +75,7 @@ function StringCat
 	do
 		RST="$RST "
 	done
-	echo "$RST";
+	echo "$RST"
 }
 function StringLine
 {
@@ -81,11 +87,15 @@ function StringLine
 	do
 		RST="$RST-"
 	done
-	echo "$RST";
+	echo "$RST"
 }
 function PrintCertInfo
 {
-	$PG_TIMEOUT 10 $PG_OPENSSL s_client -connect "$1" 2>/dev/null | $PG_OPENSSL x509 -issuer -dates -fingerprint -noout | sed 's/^/    /g'
+	str=$($PG_TIMEOUT 10 $PG_OPENSSL s_client -connect "$1" 2>/dev/null | $PG_OPENSSL x509 -issuer -dates -fingerprint -noout | sed 's/^/    /g')
+	if [ "$str" == "" ] ; then
+		exit 1
+	fi
+	echo "$str"
 }
 function GetProtocolInfo
 {
@@ -94,66 +104,137 @@ function GetProtocolInfo
 
 	ar=($str)
 	if [ "${ar[7]}" == "" ] ; then
-		line=$(StringCat "$2" "10")
+		if [ "${ar[1]}" == "" ] || [ "${ar[1]}" == "(NONE)," ] ; then
+			line=$(StringCat "$2" "10")
+		else
+			line=$(StringCat "${ar[1]:0:-1}" "10")
+		fi
 	else
 		line=$(StringCat "${ar[7]}" "10")
 	fi
-	line=$line$(StringCat "${ar[10]}" "32")
+
+	if [ "${ar[10]}" == "" ] ; then
+		if [ "${ar[4]}" == "" ] || [ "${ar[4]}" == "(NONE)" ] ; then
+			line=$line$(StringCat "" "32")
+		else
+			line=$line$(StringCat "${ar[4]}" "32")
+		fi
+	else
+		line=$line$(StringCat "${ar[10]}" "32")
+	fi
+
 	if [ "${ar[12]}" == "" ] ; then
-		line=$line$(StringCat "X" "10")
+		if [ "${ar[4]}" != "" ] && [ "${ar[4]}" != "(NONE)" ] ; then
+			line=$line$(StringCat "O" "10")
+		else
+			line=$line$(StringCat "X" "10")
+		fi
 	else
 		line=$line$(StringCat "O" "10")
 	fi
-	echo "    $line|";
+	echo "    $line|"
 }
 ################################################################################
 echo "Certification Check Script ($HOSTNAME, $SCRIPT_VERSION, $BASH_VERSION)"
 echo " "
 
-echo "Check URL - $TARGET_HOST:$TARGET_PORT"
-PrintCertInfo "$TARGET_HOST:$TARGET_PORT"
-echo ""
+if [ "$TARGET_HOST" != "" ] ; then
+	echo "Check URL - $TARGET_HOST:$TARGET_PORT"
+	PrintCertInfo "$TARGET_HOST:$TARGET_PORT"
+	echo ""
 
-echo "Report Protocol"
+	echo "Report Protocol"
 
-line=$(StringLine "" "10")
-line=$line$(StringLine "" "32")
-line=$line$(StringLine "" "10")
-echo "    $line+";
-line=$(StringCat "Protocol" "10")
-line=$line$(StringCat "Cipher" "32")
-line=$line$(StringCat "Support" "10")
-echo "    $line|";
-line=$(StringLine "" "10")
-line=$line$(StringLine "" "32")
-line=$line$(StringLine "" "10")
-echo "    $line+";
+	line=$(StringLine "" "10")
+	line=$line$(StringLine "" "32")
+	line=$line$(StringLine "" "10")
+	echo "    $line+"
+	line=$(StringCat "Protocol" "10")
+	line=$line$(StringCat "Cipher" "32")
+	line=$line$(StringCat "Support" "10")
+	echo "    $line|"
+	line=$(StringLine "" "10")
+	line=$line$(StringLine "" "32")
+	line=$line$(StringLine "" "10")
+	echo "    $line+"
 
-just=$($PG_OPENSSL s_client -help 2>&1 >/dev/null |grep -i 'just use'|awk '{print $1}')
-GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1"
-GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_1"
-GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_2"
-if [[ $just == *"-tls1_3"* ]] ; then
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_3"
+	just=$($PG_OPENSSL s_client -help 2>&1 >/dev/null |grep -i 'just use'|awk '{print $1}')
+	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1"
+	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_1"
+	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_2"
+	if [[ $just == *"-tls1_3"* ]] ; then
+		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_3"
+	fi
+	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "ssl3"
+	if [[ $just == *"-ssl2"* ]] ; then
+		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "ssl2"
+	fi
+	if [[ $just == *"-dtls1"* ]] ; then
+		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "dtls1"
+	fi
+	if [[ $just == *"-dtls1_2"* ]] ; then
+		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "dtls1_2"
+	fi
+
+	#just=$($PG_OPENSSL s_client help 2>&1 >/dev/null |grep 'just use'|awk '{print $1}')
+	#echo "$just" | while IFS=' ' read ll
+	#do
+	#	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "$ll"
+	#done
+
+	line=$(StringLine "" "10")
+	line=$line$(StringLine "" "32")
+	line=$line$(StringLine "" "10")
+	echo "    $line+"
+else
+	line=$(StringLine "" "15")
+	line=$line$(StringLine "" "16")
+	line=$line$(StringLine "" "41")
+	echo "$line+"
+	line=$(StringCat "Local Address" "15")
+	line=$line$(StringCat "PID/Program name" "16")
+	str=$(date +%Z\ %z)
+	line=$line$(StringCat "Certification Information ($str)" "41")
+	echo "$line|"
+	line=$(StringLine "" "15")
+	line=$line$(StringLine "" "16")
+	line=$line$(StringLine "" "41")
+	echo "$line+"
+
+	pplist=$(netstat -na |grep ^tcp |grep LISTEN |awk '{print $4}' | awk -F: '{print $NF}' |sort -un)
+	for pp in $pplist
+	do
+		line=$(StringCat "127.0.0.1:$pp" "15")
+		if [ "$UID" == "0" ] ; then
+			pg=$(netstat -nap |grep "^tcp" |grep LISTEN |grep ":$pp"|head -1 | awk '{print $NF}')
+		else
+			pg="-"
+		fi
+		line=$line$(StringCat "$pg" "16")
+
+		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "127.0.0.1:$pp" 2>/dev/null | $PG_OPENSSL x509 -dates -noout 2>/dev/null)
+		if [ "$str" == "" ] ; then
+			if [ "$FLAG_ALL" == "1" ] ; then
+				line=$line$(StringCat "" "41")
+				echo "$line|"
+			fi
+			continue
+		fi
+
+		pstr=$(echo "$str" | sed 's/notBefore=//g' | sed 's/notAfter=/ ~ /g' |tr -d '\n')
+		pstr1=$(date --date="${pstr:0:24}" +%Y-%m-%d\ %H:%M:%S)
+		pstr2=$(date --date="${pstr:27:24}" +%Y-%m-%d\ %H:%M:%S)
+		pstr="$pstr1 ~ $pstr2"
+
+		line=$line$(StringCat "$pstr" "41")
+
+		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "127.0.0.1:$pp" 2>/dev/null | $PG_OPENSSL x509 -issuer -noout 2>/dev/null)
+		pstr=${str//issuer=/ }
+		echo "$line|$pstr"
+	done
+
+	line=$(StringLine "" "15")
+	line=$line$(StringLine "" "16")
+	line=$line$(StringLine "" "41")
+	echo "$line+"
 fi
-GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "ssl3"
-if [[ $just == *"-ssl2"* ]] ; then
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "ssl2"
-fi
-if [[ $just == *"-dtls1"* ]] ; then
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "dtls1"
-fi
-if [[ $just == *"-dtls1_2"* ]] ; then
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "dtls1_2"
-fi
-
-#just=$($PG_OPENSSL s_client -help 2>&1 >/dev/null |grep -i 'just use'|awk '{print $1}')
-#echo "$just" | while IFS=' ' read ll
-#do
-#	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "$ll"
-#done 
-
-line=$(StringLine "" "10")
-line=$line$(StringLine "" "32")
-line=$line$(StringLine "" "10")
-echo "    $line+";
