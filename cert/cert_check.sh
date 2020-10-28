cf62963af142b6eb5c5d744d2112bfbf250c3775
요소기술팀 이석문 <smlee@sk.com>
2020-10-28 13:29:51 +0900

    o 20201008 release : 출력물 개선

--- cert/cert_check.sh
+++ cert/cert_check.sh
@@ -3,7 +3,7 @@
 # Certfication Check Script
 # 2019.09.17 created by CoreSolution (smlee@sk.com)
 ################################################################################
-SCRIPT_VERSION="20200928"
+SCRIPT_VERSION="20201008"
 LC_ALL=en_US.UTF-8
 LANG=en_US.UTF-8
 HOSTNAME=$(hostname)
@@ -69,9 +69,9 @@ function StringCat
 
 	local len_name=${#name}
 	local cnt_space=$((len-len_name))
-	local RST="| $name"
+	local RST="|$name"
 
-	for ((i=0; i<cnt_space+1; i++))
+	for ((i=0; i<cnt_space; i++))
 	do
 		RST="$RST "
 	done
@@ -83,7 +83,7 @@ function StringLine
 	local len="$2"
 	local RST="+"
 
-	for ((i=0; i<len+2; i++))
+	for ((i=0; i<len; i++))
 	do
 		RST="$RST-"
 	done
@@ -91,7 +91,7 @@ function StringLine
 }
 function PrintCertInfo
 {
-	str=$($PG_TIMEOUT 10 $PG_OPENSSL s_client -connect "$1" 2>/dev/null | $PG_OPENSSL x509 -issuer -dates -fingerprint -noout | sed 's/^/    /g')
+	str=$($PG_TIMEOUT 10 $PG_OPENSSL s_client -connect "$1" 2>/dev/null | $PG_OPENSSL x509 -issuer -subject -dates -fingerprint -noout | sed 's/^/    /g')
 	if [ "$str" == "" ] ; then
 		exit 1
 	fi
@@ -105,32 +105,32 @@ function GetProtocolInfo
 	ar=($str)
 	if [ "${ar[7]}" == "" ] ; then
 		if [ "${ar[1]}" == "" ] || [ "${ar[1]}" == "(NONE)," ] ; then
-			line=$(StringCat "$2" "10")
+			line=$(StringCat "$2" "$LEN_PROTOCOL")
 		else
-			line=$(StringCat "${ar[1]:0:-1}" "10")
+			line=$(StringCat "${ar[1]:0:-1}" "$LEN_PROTOCOL")
 		fi
 	else
-		line=$(StringCat "${ar[7]}" "10")
-	fi
-
-	if [ "${ar[10]}" == "" ] ; then
-		if [ "${ar[4]}" == "" ] || [ "${ar[4]}" == "(NONE)" ] ; then
-			line=$line$(StringCat "" "32")
-		else
-			line=$line$(StringCat "${ar[4]}" "32")
-		fi
-	else
-		line=$line$(StringCat "${ar[10]}" "32")
+		line=$(StringCat "${ar[7]}" "$LEN_PROTOCOL")
 	fi
 
 	if [ "${ar[12]}" == "" ] ; then
 		if [ "${ar[4]}" != "" ] && [ "${ar[4]}" != "(NONE)" ] ; then
-			line=$line$(StringCat "O" "10")
+			line=$line$(StringCat "  O" "$LEN_SUPPORT")
 		else
-			line=$line$(StringCat "X" "10")
+			line=$line$(StringCat "  X" "$LEN_SUPPORT")
 		fi
 	else
-		line=$line$(StringCat "O" "10")
+		line=$line$(StringCat "  O" "$LEN_SUPPORT")
+	fi
+
+	if [ "${ar[10]}" == "" ] ; then
+		if [ "${ar[4]}" == "" ] || [ "${ar[4]}" == "(NONE)" ] ; then
+			line=$line$(StringCat "" "$LEN_CIPHER")
+		else
+			line=$line$(StringCat "${ar[4]}" "$LEN_CIPHER")
+		fi
+	else
+		line=$line$(StringCat "${ar[10]}" "$LEN_CIPHER")
 	fi
 	echo "    $line|"
 }
@@ -144,18 +144,21 @@ if [ "$TARGET_HOST" != "" ] ; then
 	echo ""
 
 	echo "Report Protocol"
+	LEN_PROTOCOL=10
+	LEN_SUPPORT=5
+	LEN_CIPHER=33
 
-	line=$(StringLine "" "10")
-	line=$line$(StringLine "" "32")
-	line=$line$(StringLine "" "10")
+	line=$(StringLine "" "$LEN_PROTOCOL")
+	line=$line$(StringLine "" "$LEN_SUPPORT")
+	line=$line$(StringLine "" "$LEN_CIPHER")
 	echo "    $line+"
-	line=$(StringCat "Protocol" "10")
-	line=$line$(StringCat "Cipher" "32")
-	line=$line$(StringCat "Support" "10")
+	line=$(StringCat " Protocol " "$LEN_PROTOCOL")
+	line=$line$(StringCat "allow" "$LEN_SUPPORT")
+	line=$line$(StringCat "Cipher" "$LEN_CIPHER")
 	echo "    $line|"
-	line=$(StringLine "" "10")
-	line=$line$(StringLine "" "32")
-	line=$line$(StringLine "" "10")
+	line=$(StringLine "" "$LEN_PROTOCOL")
+	line=$line$(StringLine "" "$LEN_SUPPORT")
+	line=$line$(StringLine "" "$LEN_CIPHER")
 	echo "    $line+"
 
 	just=$($PG_OPENSSL s_client -help 2>&1 >/dev/null |grep -i 'just use'|awk '{print $1}')
@@ -182,43 +185,47 @@ if [ "$TARGET_HOST" != "" ] ; then
 	#	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "$ll"
 	#done
 
-	line=$(StringLine "" "10")
-	line=$line$(StringLine "" "32")
-	line=$line$(StringLine "" "10")
+	line=$(StringLine "" "$LEN_PROTOCOL")
+	line=$line$(StringLine "" "$LEN_SUPPORT")
+	line=$line$(StringLine "" "$LEN_CIPHER")
 	echo "    $line+"
 else
-	line=$(StringLine "" "15")
-	line=$line$(StringLine "" "16")
-	line=$line$(StringLine "" "41")
+	LEN_PORT=15
+	LEN_PID=19
+	LEN_CERT=41
+
+	line=$(StringLine "" "$LEN_PORT")
+	line=$line$(StringLine "" "$LEN_PID")
+	line=$line$(StringLine "" "$LEN_CERT")
 	echo "$line+"
-	line=$(StringCat "Local Address" "15")
-	line=$line$(StringCat "PID/Program name" "16")
+	line=$(StringCat " Local Address" "$LEN_PORT")
+	line=$line$(StringCat " PID/Program" "$LEN_PID")
 	str=$(date +%Z\ %z)
-	line=$line$(StringCat "Certification Information ($str)" "41")
+	line=$line$(StringCat " Certification Information ($str)" "$LEN_CERT")
 	echo "$line|"
-	line=$(StringLine "" "15")
-	line=$line$(StringLine "" "16")
-	line=$line$(StringLine "" "41")
+	line=$(StringLine "" "$LEN_PORT")
+	line=$line$(StringLine "" "$LEN_PID")
+	line=$line$(StringLine "" "$LEN_CERT")
 	echo "$line+"
 
 	pplist=$(netstat -na |grep ^tcp |grep LISTEN |awk '{print $4}' | awk -F: '{print $NF}' |sort -un)
 	for pp in $pplist
 	do
-		line=$(StringCat "127.0.0.1:$pp" "15")
+		line=$(StringCat "127.0.0.1:$pp" "$LEN_PORT")
 		if [ "$UID" == "0" ] ; then
 			pg=$(netstat -nap |grep "^tcp" |grep LISTEN |grep ":$pp"|head -1 | awk '{print $NF}')
-			if [[ $pg != *"/"* ]] ; then	
+			if [[ $pg != *"/"* ]] ; then
 				pg=$(netstat -nap |grep "^tcp" |grep LISTEN |grep ":$pp"|head -1 | awk '{print $7}')
 			fi
 		else
 			pg="-"
 		fi
-		line=$line$(StringCat "$pg" "16")
+		line=$line$(StringCat "$pg" "$LEN_PID")
 
 		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "127.0.0.1:$pp" 2>/dev/null | $PG_OPENSSL x509 -dates -noout 2>/dev/null)
 		if [ "$str" == "" ] ; then
 			if [ "$FLAG_ALL" == "1" ] ; then
-				line=$line$(StringCat "" "41")
+				line=$line$(StringCat "" "$LEN_CERT")
 				echo "$line|"
 			fi
 			continue
@@ -229,15 +236,15 @@ else
 		pstr2=$(date --date="${pstr:27:24}" +%Y-%m-%d\ %H:%M:%S)
 		pstr="$pstr1 ~ $pstr2"
 
-		line=$line$(StringCat "$pstr" "41")
+		line=$line$(StringCat "$pstr" "$LEN_CERT")
 
 		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "127.0.0.1:$pp" 2>/dev/null | $PG_OPENSSL x509 -issuer -noout 2>/dev/null)
 		pstr=${str//issuer=/ }
 		echo "$line|$pstr"
 	done
 
-	line=$(StringLine "" "15")
-	line=$line$(StringLine "" "16")
-	line=$line$(StringLine "" "41")
+	line=$(StringLine "" "$LEN_PORT")
+	line=$line$(StringLine "" "$LEN_PID")
+	line=$line$(StringLine "" "$LEN_CERT")
 	echo "$line+"
 fi
