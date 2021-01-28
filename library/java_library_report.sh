fe2971d3d9d4eb772131c6d070a02166d6d12a81
요소기술팀 이석문 <smlee@sk.com>
2021-01-28 15:09:15 +0900

    o 20210128 : class, jsp 파일 비교 옵션/기능 추가

--- library/java_library_report.sh
+++ library/java_library_report.sh
@@ -3,7 +3,7 @@
 ## Java Library Report Script
 ## 2020.11 created by smlee@sk.com
 ################################################################################
-SCRIPT_VERSION="20201230"
+SCRIPT_VERSION="20210128"
 LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
 HOSTNAME=$(hostname)
@@ -14,6 +14,8 @@ if [ -d "$TMPDIR" ] ; then rm -rf "$TMPDIR"; fi # tmp code
 PROGRAM_FILE1=""
 PROGRAM_FILE2=""
 FLAG_ALL=0
+FLAG_CLASS=0
+FLAG_JSP=0
 while [ "$#" -gt 0 ] ; do
 case "$1" in
 	-v|--version)
@@ -30,6 +32,14 @@ case "$1" in
 		;;
 	-a|--all)
 		FLAG_ALL=1
+		shift 1
+		;;
+	-c|--class)
+		FLAG_CLASS=1
+		shift 1
+		;;
+	-j|--jsp)
+		FLAG_JSP=1
 		shift 1
 		;;
 	 *)
@@ -161,11 +171,15 @@ if [ "$?" != "0" ] ; then echo "fail to make directory($TMPDIR/file2/)"; exit 1;
 unzip "$PROGRAM_FILE1" -d "$TMPDIR/file1/" &> /dev/null
 if [ "$?" != "0" ] ; then echo "unzip error $PROGRAM_FILE1 ($?)"; exit 1; fi
 find "$TMPDIR/file1/" -type f -name "*.jar" -exec md5sum {} \; |awk '{print $2,$1}' | sort > "$TMPDIR/file1.jar.txt"
+find "$TMPDIR/file1/" -type f -name "*.class" -exec md5sum {} \; |awk '{print $2,$1}' | sort > "$TMPDIR/file1.class.txt"
+find "$TMPDIR/file1/" -type f -name "*.jsp" -exec md5sum {} \; |awk '{print $2,$1}' | sort > "$TMPDIR/file1.jsp.txt"
 
 if [ "$PROGRAM_FILE2" != "" ] ; then
 	unzip "$PROGRAM_FILE2" -d "$TMPDIR/file2/" &> /dev/null
 	if [ "$?" != "0" ] ; then echo "unzip error $PROGRAM_FILE2 ($?)"; exit 1; fi
 	find "$TMPDIR/file2/" -type f -name "*.jar" -exec md5sum {} \; |awk '{print $2,$1}' | sort > "$TMPDIR/file2.jar.txt"
+	find "$TMPDIR/file2/" -type f -name "*.class" -exec md5sum {} \; |awk '{print $2,$1}' | sort > "$TMPDIR/file2.class.txt"
+	find "$TMPDIR/file2/" -type f -name "*.jsp" -exec md5sum {} \; |awk '{print $2,$1}' | sort > "$TMPDIR/file2.jsp.txt"
 fi
 
 echo " Java Library Report Script ($HOSTNAME, $SCRIPT_VERSION, $BASH_VERSION)"
@@ -256,11 +270,15 @@ if [ "$PROGRAM_FILE2" != "" ] ; then
 		len=$((${#fn}-${#DIR}))
 		if [ $len -gt "$LEN_FILE2" ] ; then LEN_FILE2=$len; fi
 	done < <(cat "$TMPDIR/file2.jar.txt")
-	CATFILE="$TMPDIR/file2.jar.txt"
+	JARFILE="$TMPDIR/file2.jar.txt"
+	CLASSFILE="$TMPDIR/file2.class.txt"
+	JSPFILE="$TMPDIR/file2.jsp.txt"
 else
 	LEN_FILE2=10
 	LEN_MEMO=32
-	CATFILE="$TMPDIR/file1.jar.txt"
+	JARFILE="$TMPDIR/file1.jar.txt"
+	CLASSFILE="$TMPDIR/file1.class.txt"
+	JSPFILE="$TMPDIR/file1.jsp.txt"
 fi
 
 line=$(StringLine "" "$LEN_FILE1")
@@ -281,88 +299,99 @@ line=$line$(StringLine "" "$LEN_FILE2")
 line=$line$(StringLine "" "$LEN_MEMO")
 echo "$line+"
 
-while IF=" " read fn chksum
-do
-	fnbase=$(basename "$fn")
-
-	if [ "$PROGRAM_FILE2" == "" ] ; then
-		DIR="$TMPDIR/file1/"
-		len=${#DIR}
-		pfilename1=${fn:$len}
-		FN_SIZE=$(GetFileSize "$fn")
-
-		line=$(StringCat "$pfilename1" "$LEN_FILE1")
-		line=$line$(StringCat "$FN_SIZE" "$LEN_FILE2" "right")
-		line=$line$(StringCat "$chksum" "$LEN_MEMO")
-		echo "$line|"
-		continue
-	fi
-
-	FLAG=0
-	fnold=
-	while IF=" " read a b
+function JavaReportLibrary
+{
+	while IF=" " read fn chksum
 	do
-		fnbase2=$(basename "$a")
-		if [ "$fnbase" == "$fnbase2" ] ; then
-			fnold=$a
-			if [ "$chksum" == "$b" ] ; then
-				FLAG=1
-			else
-				FLAG=2
-			fi
-		fi
-	done < <(grep "$fnbase" "$TMPDIR/file1.jar.txt")
+		fnbase=$(basename "$fn")
 
-	if [ "$FLAG_ALL" == "0" ] && [ "$FLAG" == "1" ] ; then continue; fi
-
-	if [ "$fnold" != "" ] ; then
-		DIR="$TMPDIR/file1/"
-		len=${#DIR}
-		pfilename1=${fnold:$len}
-	else
-		DIR="$TMPDIR/file2/"
-		len=${#DIR}
-		pfilename0=${fn:$len}
-		rver=$(echo "$pfilename0" |awk -F- '{print $NF}')
-		len=$((${#pfilename0}-${#rver}))
-		rname=${pfilename0:0:$len}
-
-		lfile=$(grep "$rname" "$TMPDIR/file1.jar.txt" |sort |head -1)
-		if [ "$lfile" != "" ] ; then
-			FLAG=3
+		if [ "$PROGRAM_FILE2" == "" ] ; then
 			DIR="$TMPDIR/file1/"
 			len=${#DIR}
-			fnold=$(echo "$lfile" |awk '{print $1}')
+			pfilename1=${fn:$len}
+			FN_SIZE=$(GetFileSize "$fn")
+
+			line=$(StringCat "$pfilename1" "$LEN_FILE1")
+			line=$line$(StringCat "$FN_SIZE" "$LEN_FILE2" "right")
+			line=$line$(StringCat "$chksum" "$LEN_MEMO")
+			echo "$line|"
+			continue
+		fi
+
+		FLAG=0
+		fnold=
+		while IF=" " read a b
+		do
+			fnbase2=$(basename "$a")
+			if [ "$fnbase" == "$fnbase2" ] ; then
+				fnold=$a
+				if [ "$chksum" == "$b" ] ; then
+					FLAG=1
+				else
+					FLAG=2
+				fi
+			fi
+		done < <(grep "$fnbase" "$2")
+
+		if [ "$FLAG_ALL" == "0" ] && [ "$FLAG" == "1" ] ; then continue; fi
+
+		if [ "$fnold" != "" ] ; then
+			DIR="$TMPDIR/file1/"
+			len=${#DIR}
 			pfilename1=${fnold:$len}
 		else
-			pfilename1=
-		fi
-	fi
-	DIR="$TMPDIR/file2/"
-	len=${#DIR}
-	pfilename2=${fn:$len}
+			DIR="$TMPDIR/file2/"
+			len=${#DIR}
+			pfilename0=${fn:$len}
+			rver=$(echo "$pfilename0" |awk -F- '{print $NF}')
+			len=$((${#pfilename0}-${#rver}))
+			rname=${pfilename0:0:$len}
 
-	STR=
-	if [ "$FLAG" == "0" ] ; then
-		STR="NEW(+)"
-	elif [ "$FLAG" == "1" ] ; then
-		STR="SAME(=)"
-	elif [ "$FLAG" == "2" ] ; then
-		STR="MODIFY(*)"
-	elif [ "$FLAG" == "3" ] ; then
-		STR=$(VerDiff "$pfilename1" "$pfilename2")
-		if [ "$STR" == "upgrade" ] ; then
-			STR="REPLACE(>)"
-		else
-			STR="REPLACE(<)"
+			lfile=$(grep "$rname" "$2" |sort |head -1)
+			if [ "$lfile" != "" ] ; then
+				FLAG=3
+				DIR="$TMPDIR/file1/"
+				len=${#DIR}
+				fnold=$(echo "$lfile" |awk '{print $1}')
+				pfilename1=${fnold:$len}
+			else
+				pfilename1=
+			fi
 		fi
-	fi
+		DIR="$TMPDIR/file2/"
+		len=${#DIR}
+		pfilename2=${fn:$len}
 
-	line=$(StringCat "$pfilename1" "$LEN_FILE1")
-	line=$line$(StringCat "$pfilename2" "$LEN_FILE2")
-	line=$line$(StringCat "$STR" "$LEN_MEMO")
-	echo "$line|"
-done < <(cat "$CATFILE")
+		STR=
+		if [ "$FLAG" == "0" ] ; then
+			STR="NEW(+)"
+		elif [ "$FLAG" == "1" ] ; then
+			STR="SAME(=)"
+		elif [ "$FLAG" == "2" ] ; then
+			STR="MODIFY(*)"
+		elif [ "$FLAG" == "3" ] ; then
+			STR=$(VerDiff "$pfilename1" "$pfilename2")
+			if [ "$STR" == "upgrade" ] ; then
+				STR="REPLACE(>)"
+			else
+				STR="REPLACE(<)"
+			fi
+		fi
+
+		line=$(StringCat "$pfilename1" "$LEN_FILE1")
+		line=$line$(StringCat "$pfilename2" "$LEN_FILE2")
+		line=$line$(StringCat "$STR" "$LEN_MEMO")
+		echo "$line|"
+	done < <(cat "$1")
+}
+
+JavaReportLibrary "$JARFILE" "$TMPDIR/file1.jar.txt"
+if [ "$FLAG_CLASS" == "1" ] ; then
+	JavaReportLibrary "$CLASSFILE" "$TMPDIR/file1.class.txt"
+fi
+if [ "$FLAG_JSP" == "1" ] ; then
+	JavaReportLibrary "$JSPFILE" "$TMPDIR/file1.jsp.txt"
+fi
 
 line=$(StringLine "" "$LEN_FILE1")
 line=$line$(StringLine "" "$LEN_FILE2")
