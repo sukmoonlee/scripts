90d4ac4b1ea3d123b6c394bfe502185f861dc000
요소기술팀 이석문 <smlee@sk.com>
2021-05-27 13:27:05 +0900

    o 20210525 : 코드 정리 및 기능 개선

--- cert/cert_check.sh
+++ cert/cert_check.sh
@@ -3,7 +3,7 @@
 # Certfication Check Script
 # 2020.12.26 created by smlee@sk.com
 ################################################################################
-SCRIPT_VERSION="20210221"
+SCRIPT_VERSION="20210525"
 LC_ALL=en_US.UTF-8
 LANG=en_US.UTF-8
 HOSTNAME=$(hostname)
@@ -69,15 +69,19 @@ function _stringCat
 {
 	local name="$1"
 	local len="$2"
+	local align="$3"
 
 	local len_name=${#name}
 	local cnt_space=$((len-len_name))
 	local RST="|$name"
 
+	if [ "$align" == "right" ] ; then RST="|"; fi
 	for ((i=0; i<cnt_space; i++))
 	do
 		RST="$RST "
 	done
+
+	if [ "$align" == "right" ] ; then RST="$RST$name"; fi
 	echo "$RST"
 }
 function _stringLine
@@ -96,11 +100,20 @@ _STRING_NO=0
 _STRING_LIST=()
 function makeString
 {
-	if [ "$2" == "newline" ] ; then
+	if [ "$2" == "new" ] ; then
+		_STRING_LIST=()
+		_STRING_NO=0
+		_STRING_LIST[$_STRING_NO]=""
+	elif [ "$2" == "newline" ] ; then
 		_STRING_NO=$((_STRING_NO+1))
 		_STRING_LIST[$_STRING_NO]=""
 	fi
-	_STRING_LIST[$_STRING_NO]="${_STRING_LIST[$_STRING_NO]}$1|"
+
+	if [ "$3" == "right" ] ; then
+		_STRING_LIST[$_STRING_NO]="${_STRING_LIST[$_STRING_NO]}$1~R|"
+	else
+		_STRING_LIST[$_STRING_NO]="${_STRING_LIST[$_STRING_NO]}$1|"
+	fi
 }
 function printString
 {
@@ -112,8 +125,12 @@ function printString
 			if [ "${SLEN[$_col_cnt]}" == "" ] ; then
 				SLEN[$_col_cnt]=0
 			fi
-			if [ "${#_col}" -gt "${SLEN[$_col_cnt]}" ] ; then
-				SLEN[$_col_cnt]=${#_col}
+			local _len=${#_col}
+			if [ "${_col:$((_len-2)):2}" == "~R" ] ; then
+				_len=$((_len-2))
+			fi
+			if [ "$_len" -gt "${SLEN[$_col_cnt]}" ] ; then
+				SLEN[$_col_cnt]=$_len
 			fi
 			_col_cnt=$((_col_cnt+1))
 		done
@@ -129,7 +146,12 @@ function printString
 				_line=$_line$(_stringLine "" "${SLEN[$_col_cnt]}")
 				_last_flag=1
 			else
-				_line=$_line$(_stringCat "$_col" "${SLEN[$_col_cnt]}")
+				local _len=${#_col}
+				if [ "${_col:$((_len-2)):2}" == "~R" ] ; then
+					_line=$_line$(_stringCat "${_col:0:$((_len-2))}" "${SLEN[$_col_cnt]}" "right")
+				else
+					_line=$_line$(_stringCat "$_col" "${SLEN[$_col_cnt]}")
+				fi
 				_last_flag=0
 				if [ "${_col:0:4}" == " >>>" ] ; then _last_flag=2; fi
 			fi
@@ -185,131 +207,118 @@ function GetProtocolInfo
 		makeString " ${ar[10]} "
 	fi
 }
-################################################################################
-echo "Certification Check Script ($HOSTNAME, $SCRIPT_VERSION, $BASH_VERSION)"
-echo " "
+function GetTlsHostPort
+{
+	local _addr="$1"
 
-if [ "$TARGET_HOST" != "" ] ; then
-	echo "Check URL - $TARGET_HOST:$TARGET_PORT"
-	PrintCertInfo "$TARGET_HOST:$TARGET_PORT"
+	echo "Check URL - $_addr"
+	PrintCertInfo "$_addr"
 	echo ""
 
 	echo "Report Protocol"
-	makeString "-" "newline"; makeString "-"; makeString "-";
+	makeString "-" "new"; makeString "-"; makeString "-";
 	makeString " Protocol " "newline"
 	makeString " allow "
 	makeString " Cipher "
 	makeString "-" "newline"; makeString "-"; makeString "-";
 
 	just=$($PG_OPENSSL s_client -help 2>&1 >/dev/null |grep -ai 'just use'|awk '{print $1}')
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1"
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_1"
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_2"
+	GetProtocolInfo "$_addr" "tls1"
+	GetProtocolInfo "$_addr" "tls1_1"
+	GetProtocolInfo "$_addr" "tls1_2"
 	if [[ $just == *"-tls1_3"* ]] ; then
-		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "tls1_3"
+		GetProtocolInfo "$_addr" "tls1_3"
 	fi
-	GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "ssl3"
+	GetProtocolInfo "$_addr" "ssl3"
 	if [[ $just == *"-ssl2"* ]] ; then
-		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "ssl2"
+		GetProtocolInfo "$_addr" "ssl2"
 	fi
 	if [[ $just == *"-dtls1"* ]] ; then
-		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "dtls1"
+		GetProtocolInfo "$_addr" "dtls1"
 	fi
 	if [[ $just == *"-dtls1_2"* ]] ; then
-		GetProtocolInfo "$TARGET_HOST:$TARGET_PORT" "dtls1_2"
+		GetProtocolInfo "$_addr" "dtls1_2"
 	fi
 
 	makeString "-" "newline"; makeString "-"; makeString "-";
 	printString |tee |sed 's/^/    /'
-elif [ "$FLAG_REPORT" == "1" ] ; then
-	makeString "-" "newline"; makeString "-"; makeString "-";
-	makeString " Test Socket " "newline"
-	makeString " PID/Program "
-	makeString " Certification Information ($(date +%Z\ %z)) "
-	makeString "-" "newline"; makeString "-"; makeString "-";
+}
+################################################################################
+sslversion=$($PG_OPENSSL version)
+echo "Certification Check Script ($HOSTNAME, $SCRIPT_VERSION, $BASH_VERSION, $sslversion)"
+echo ""
 
-	pplist=$($PG_NETSTAT -na |grep ^tcp |grep LISTEN |awk '{print $4}' | awk -F: '{print $NF}' |sort -un)
-	for pp in $pplist
-	do
-		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "127.0.0.1:$pp" 2>/dev/null | $PG_OPENSSL x509 -dates -noout 2>/dev/null)
-		if [ "$str" == "" ] ; then continue; fi
-		makeString " 127.0.0.1:$pp " "newline"
+if [ "$TARGET_HOST" != "" ] ; then
+	GetTlsHostPort "$TARGET_HOST:$TARGET_PORT"
+	exit 0
+fi
 
-		if [ "$UID" == "0" ] ; then
-			pg=$($PG_NETSTAT -nap |grep "^tcp" |grep LISTEN |grep ":$pp"|head -1 | awk '{print $7}')
-		else
-			pg="-"
-		fi
-		makeString " $pg "
+makeString "-" "newline"; makeString "-"; makeString "-"; makeString "-";
+makeString " Listen Socket " "newline"
+makeString " Test Socket "
+makeString " PID/Program "
+makeString " Certification Information ($(date +%Z\ %z)) "
+makeString "-" "newline"; makeString "-"; makeString "-"; makeString "-";
 
+pplist=$($PG_NETSTAT -nap 2>/dev/null |grep ^tcp |grep LISTEN|sort -n)
+pparray=()
+cnt=0
+while read pp
+do
+	cnt=$((cnt+1))
+	pparray[cnt]=$pp
+done <<< "$pplist"
+
+report_array=()
+cnt=0
+for i in $(seq 1 ${#pparray[@]})
+do
+	IFS=' ' read -r -a _array <<< "${pparray[$i]}"
+
+	str_ip=$(echo "${_array[3]}" | awk -F: '{print substr($0,0,length($0)-length($NF)-1)}')
+	str_port=$(echo "${_array[3]}" | awk -F: '{print $NF}')
+	if [ "${_array[0]}" == "tcp" ] && [ "$str_ip" == "0.0.0.0" ] ; then
+		str_addr="127.0.0.1:$str_port"
+	elif [ "$str_ip" == "::" ] || [ "$str_ip" == "::1" ] ; then
+		str_addr="[::1]:$str_port"
+	elif [ "${_array[0]}" == "tcp6" ] ; then
+		str_addr="[$str_ip]:$str_port"
+	else
+		str_addr="$str_ip:$str_port"
+	fi
+
+	str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "$str_addr" 2>/dev/null | $PG_OPENSSL x509 -dates -noout 2>/dev/null)
+	if [ "$str" == "" ] ; then
+		if [ "$FLAG_REPORT" == "1" ] ; then continue; fi
+		pstr=
+	else
 		pstr=$(echo "$str" | sed 's/notBefore=//g' | sed 's/notAfter=/ ~ /g' |tr -d '\n')
 		pstr1=$(date --date="${pstr:0:24}" +%Y-%m-%d\ %H:%M:%S)
 		pstr2=$(date --date="${pstr:27:24}" +%Y-%m-%d\ %H:%M:%S)
 		pstr="$pstr1 ~ $pstr2"
-		makeString " $pstr "
-
-		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "127.0.0.1:$pp" 2>/dev/null | $PG_OPENSSL x509 -issuer -noout 2>/dev/null)
-		pstr1=${str//issuer=/ }
-		if [ "$pstr1" != "" ] ; then makeString " >>>$pstr1"; fi
-	done
-
-	makeString "-" "newline"; makeString "-"; makeString "-";
-	printString
-else
-	makeString "-" "newline"; makeString "-"; makeString "-"; makeString "-";
-	makeString " Listen Socket " "newline"
-	makeString " Test Socket "
-	makeString " PID/Program "
-	makeString " Certification Information ($(date +%Z\ %z)) "
-	makeString "-" "newline"; makeString "-"; makeString "-"; makeString "-";
-
-	pplist=$($PG_NETSTAT -nap 2>/dev/null |grep ^tcp |grep LISTEN)
-	pparray=()
-	cnt=0
-	while read pp
-	do
 		cnt=$((cnt+1))
-		pparray[cnt]=$pp
-	done <<< "$pplist"
+		report_array[cnt]=$str_addr
+	fi
 
-	for i in $(seq 1 ${#pparray[@]})
-	do
-		IFS=' ' read -r -a _array <<< "${pparray[$i]}"
-		makeString " ${_array[3]} " "newline"
+	makeString " ${_array[3]} " "newline"
+	makeString " $str_addr "
+	makeString " ${_array[6]} "
+	makeString " $pstr "
 
-		str_ip=$(echo "${_array[3]}" | awk -F: '{print substr($0,0,length($0)-length($NF)-1)}')
-		str_port=$(echo "${_array[3]}" | awk -F: '{print $NF}')
-		if [ "${_array[0]}" == "tcp" ] && [ "$str_ip" == "0.0.0.0" ] ; then
-			str_addr="127.0.0.1:$str_port"
-		elif [ "${_array[0]}" == "tcp6" ] && [ "$str_ip" == "::" ] ; then
-			str_addr="[::1]:$str_port"
-		elif [ "${_array[0]}" == "tcp" ] ; then
-			str_addr="$str_ip:$str_port"
-		elif [ "${_array[0]}" == "tcp6" ] ; then
-			str_addr="[$str_ip]:$str_port"
-		fi
-		makeString " $str_addr "
-		makeString " ${_array[6]} "
-
-		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "$str_addr" 2>/dev/null | $PG_OPENSSL x509 -dates -noout 2>/dev/null)
-		if [ "$str" == "" ] ; then
-			makeString ""
-			continue
-		fi
-
-		pstr=$(echo "$str" | sed 's/notBefore=//g' | sed 's/notAfter=/ ~ /g' |tr -d '\n')
-		pstr1=$(date --date="${pstr:0:24}" +%Y-%m-%d\ %H:%M:%S)
-		pstr2=$(date --date="${pstr:27:24}" +%Y-%m-%d\ %H:%M:%S)
-		pstr="$pstr1 ~ $pstr2"
-		makeString " $pstr "
-
+	if [ "$pstr" != "" ] ; then
 		str=$($PG_TIMEOUT 1 $PG_OPENSSL s_client -connect "$str_addr" 2>/dev/null | $PG_OPENSSL x509 -issuer -noout 2>/dev/null)
 		pstr1=${str//issuer=/ }
 		if [ "$pstr1" != "" ] ; then makeString " >>>$pstr1"; fi
-	done
+	fi
+done
 
-	makeString "-" "newline"; makeString "-"; makeString "-"; makeString "-";
-	printString
-fi
+makeString "-" "newline"; makeString "-"; makeString "-"; makeString "-";
+printString
+
+for i in $(seq 1 ${#report_array[@]})
+do
+	echo ""
+	GetTlsHostPort "${report_array[$i]}"
+done
 
 exit 0
